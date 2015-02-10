edc1b73ba0a1e28606ae4185e8e753b122b03fd4
Spencer Morris <spencermorris@Spencers-MacBook-Air.local>
2015-02-10 08:51:59 -0600

    Initial Commit

---
+++ PaceCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaceCalculator
+//
+//  Created by Spencer Morris on 2/10/15.
+//  Copyright (c) 2015 Spencer Morris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
